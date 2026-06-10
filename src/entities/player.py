76ac1d169bc0790c9c36c57403023e3fa2380c21
yuunp <pacanzachristopher@gmail.com>
2026-06-10 12:51:21 +0800

    Organized the folders and also added three new walking animations left, forward and backward

--- src/entities/player.py
+++ src/entities/player.py
@@ -7,7 +7,7 @@ pygame.init()
 screen = pygame.display.set_mode((400, 300))  
 pygame.display.set_caption("Walk Animation")
 
-frames = [pygame.image.load(f"assets/images/frames/main_character/idle_right/frame_{i}.png").convert_alpha() for i in range(8)]
+frames = [pygame.image.load(f"assets/images/frames/main_character/walking/walking_up/frame_{i}.png").convert_alpha() for i in range(8)]
 frames = [pygame.transform.scale(f, (f.get_width() // 4, f.get_height() // 4)) for f in frames] 
 
 screen = pygame.display.set_mode((frames[0].get_width() + 40, frames[0].get_height() + 40))

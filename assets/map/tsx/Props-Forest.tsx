d77235b117e7d11e2187f389b56d47a3ee182cda
Theejay <th.tagama@gmail.com>
2026-08-08 17:42:06 +0800

    fix collisions

--- assets/map/tsx/Props-Forest.tsx
+++ assets/map/tsx/Props-Forest.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="Props-Forest" tilewidth="16" tileheight="16" tilecount="56" columns="7">
+<tileset version="1.10" tiledversion="1.12.2" name="Props-Forest" tilewidth="16" tileheight="16" tilecount="56" columns="7">
  <image source="../tiledsets/Props-Forest.png" width="112" height="128"/>
  <tile id="8">
   <properties>

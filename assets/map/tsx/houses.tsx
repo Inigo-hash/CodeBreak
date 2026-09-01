e420b73362d238879c2dc2f2e004cf3fdcedb3ed
Inigo <inigoagunod457@gmail.com>
2026-09-01 18:06:43 +0800

    fixed image source for houses.tsx and houses.png

--- assets/map/tsx/houses.tsx
+++ assets/map/tsx/houses.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.1" name="houses" tilewidth="16" tileheight="16" tilecount="4096" columns="64">
- <image source="D:/Downloads/Houses_Pack/houses.png" width="1024" height="1024"/>
+ <image source="../tiledsets/houses.png" width="1024" height="1024"/>
  <tile id="2008">
   <properties>
    <property name="collidable" type="bool" value="true"/>

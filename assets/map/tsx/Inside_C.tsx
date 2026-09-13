e5eab1ddf09946c66d29799667ad91535021ab04
Inigo <inigoagunod457@gmail.com>
2026-09-13 18:16:30 +0800

    Added more tilesets

---
+++ assets/map/tsx/Inside_C.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="Inside_C" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="../tiledsets/Inside_C.png" width="512" height="512"/>
+</tileset>
